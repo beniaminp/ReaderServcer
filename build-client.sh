cf1ba92083a79fbc71a6dfe46db1a8b7e6d42362
Dragos-Beniamin Pantiru <beniaminpantiru@gmail.com>
2019-11-13 11:13:05 +0000

    build-client

--- build-client.sh
+++ build-client.sh
@@ -1,4 +1,7 @@
 cd ebook-client
 npm install
+ionic cordova platform rm browser
+ionic cordova platform add browser
 ionic cordova build browser
+rm -rf ../src/main/resources/public/*
 cp -R platforms/browser/www/ ../src/main/resources/public/
