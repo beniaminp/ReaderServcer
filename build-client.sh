7602b04b03e85634c6bac20143a9029f984404a7
Dragos-Beniamin Pantiru <beniaminpantiru@gmail.com>
2019-11-13 11:19:29 +0000

    fixed build

--- build-client.sh
+++ build-client.sh
@@ -4,4 +4,4 @@ ionic cordova platform rm browser
 ionic cordova platform add browser
 ionic cordova build browser
 rm -rf ../src/main/resources/public/*
-cp -R platforms/browser/www/ ../src/main/resources/public/
+cp -R platforms/browser/www/* ../src/main/resources/public/
