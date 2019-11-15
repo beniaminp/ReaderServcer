05f3e1051232f31988c4d7fcbfef0fcd48e93b22
Dragos-Beniamin Pantiru <beniaminpantiru@gmail.com>
2019-11-15 08:38:01 +0200

    mkdir

--- build-server.sh
+++ build-server.sh
@@ -1,7 +1,7 @@
 echo 'stop server'
 ./stop.sh
 echo 'create old versions dir'
-mkdir /home/beniaminp/sublime/old_versions
+mkdir -p /home/beniaminp/sublime/old_versions
 echo 'move current app to old version dir'
 mv /home/beniaminp/sublime/ebook-0.0.1-SNAPSHOT.jar /home/beniaminp/sublime/old_versions/
 echo 'copy new version'
